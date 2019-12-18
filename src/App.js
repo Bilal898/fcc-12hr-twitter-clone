97cc37bcdfc4eaf37f63b35b1a8bf2284cad38e1
Bilal989 <bfhamid@gmail.com>
2019-12-18 14:00:14 +0500

    6.56 mui version downgrade. theme is working in pages. refactor authenticated to redux

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import themeFile from "./util/theme";
 import jwtDecode from "jwt-decode";
+import axios from "axios";
+//redux
+import { SET_AUTHENTICATED } from "./redux/types";
+import { logout, getUserData, logoutUser } from "./redux/actions/userActions";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
@@ -14,48 +18,44 @@ import home from "./pages/home";
 import login from "./pages/login";
 import signup from "./pages/signup";
 //mui
-import ThemeProvider from "@material-ui/styles/ThemeProvider";
+// import ThemeProvider from "@material-ui/styles/ThemeProvider";
+// import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
 const theme = createMuiTheme(themeFile);
 
-let authenticated;
+// let authenticated;
 const token = localStorage.FBIdToken;
 if (token) {
   const decodedToken = jwtDecode(token);
   if (decodedToken.exp * 1000 < Date.now()) {
+    store.dispatch(logoutUser());
     window.location.href = "/login";
-    authenticated = false;
+    // authenticated = false;
   } else {
-    authenticated = true;
+    // authenticated = true;
+    store.dispatch({ type: SET_AUTHENTICATED });
+    axios.defaults.headers.common["Authorization"] = token;
+    store.dispatch(getUserData());
   }
 }
 export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <ThemeProvider theme={theme}>
+        <MuiThemeProvider theme={theme}>
           <Router>
             <Navbar />
             <div className="container">
               <Switch>
                 <Route exact path="/" component={home} />
-                <AuthRoute
-                  exact
-                  path="/login"
-                  component={login}
-                  authenticated={authenticated}
-                />
-                <AuthRoute
-                  exact
-                  path="/signup"
-                  component={signup}
-                  authenticated={authenticated}
-                />
+                <AuthRoute exact path="/login" component={login} />
+                <AuthRoute exact path="/signup" component={signup} />
               </Switch>
             </div>
           </Router>
-        </ThemeProvider>
+        </MuiThemeProvider>
       </Provider>
     );
   }
--- src/pages/login.js
+++ src/pages/login.js
@@ -8,40 +8,39 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import axios from "axios";
 import CircularProgress from "@material-ui/core/CircularProgress";
 //redux
 import { connect } from "react-redux";
 import { loginUser } from "../redux/actions/userActions";
-// const styles = theme => ({
-//   ...theme.spreadIt
-// });
-const styles = {
-  form: {
-    textAlign: "center"
-  },
-  image: {
-    margin: "20px auto 20px auto"
-  },
-  pageTitle: {
-    margin: "10px auto 10px auto"
-  },
-  textField: {
-    margin: "10px auto 10px auto"
-  },
-  button: {
-    marginTop: 20,
-    position: "relative"
-  },
-  customError: {
-    color: "red",
-    fontSize: "0.8rem",
-    marginTop: 10
-  },
-  progress: {
-    position: "absolute"
-  }
-};
+const styles = theme => ({
+  ...theme
+});
+// const styles = {
+//   form: {
+//     textAlign: "center"
+//   },
+//   image: {
+//     margin: "20px auto 20px auto"
+//   },
+//   pageTitle: {
+//     margin: "10px auto 10px auto"
+//   },
+//   textField: {
+//     margin: "10px auto 10px auto"
+//   },
+//   button: {
+//     marginTop: 20,
+//     position: "relative"
+//   },
+//   customError: {
+//     color: "red",
+//     fontSize: "0.8rem",
+//     marginTop: 10
+//   },
+//   progress: {
+//     position: "absolute"
+//   }
+// };
 
 class login extends Component {
   state = {

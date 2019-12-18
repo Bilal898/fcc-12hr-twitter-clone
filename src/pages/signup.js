707b1e5384fb3d8a92ca188cd21baebc090ec41b
Bilal989 <bfhamid@gmail.com>
2019-12-18 13:16:44 +0500

    6.49 signup

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -10,6 +10,8 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import axios from "axios";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import { connect } from "react-redux";
+import { signupUser } from "../redux/actions/userActions";
 
 const styles = {
   form: {
@@ -44,33 +46,24 @@ class signup extends Component {
     password: "",
     confirmPassword: "",
     handle: "",
-    loading: false,
     errors: {}
   };
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.UI.errors) {
+      this.setState({ errors: nextProps.UI.errors });
+    }
+  }
   handleSubmit = event => {
     event.preventDefault();
-    this.setState({ loading: true });
+    // this.setState({ loading: true });
     const newUserData = {
       email: this.state.email,
       password: this.state.password,
       confirmPassword: this.state.confirmPassword,
       handle: this.state.handle
     };
-    axios
-      .post("/login", newUserData)
-      .then(res => {
-        console.log(res.data);
-        localStorage.setItem("FBIdToken", `Bearer ${res.data.token}`);
-        this.setState({ loading: false });
-        this.props.history.push("/");
-      })
-      .catch(err => {
-        this.setState({
-          errors: err.response.data,
-          loading: false
-        });
-      });
+    this.props.signupUser(newUserData, this.props.history);
   };
   handleChange = event => {
     this.setState({
@@ -78,8 +71,11 @@ class signup extends Component {
     });
   };
   render() {
-    const { classes } = this.props;
-    const { errors, loading } = this.state;
+    const {
+      classes,
+      UI: { loading }
+    } = this.props;
+    const { errors } = this.state;
     return (
       <Grid container className={classes.form}>
         <Grid item sm />
@@ -114,14 +110,14 @@ class signup extends Component {
               fullWidth
             />
             <TextField
-              id="confimPassword"
-              name="confimPassword"
+              id="confirmPassword"
+              name="confirmPassword"
               type="password"
               label="Confirm Password"
               className={classes.textField}
-              helperText={errors.confimPassword}
-              error={errors.confimPassword ? true : false}
-              value={this.state.confimPassword}
+              helperText={errors.confirmPassword}
+              error={errors.confirmPassword ? true : false}
+              value={this.state.confirmPassword}
               onChange={this.handleChange}
               fullWidth
             />
@@ -167,4 +163,18 @@ class signup extends Component {
   }
 }
 
-export default withStyles(styles)(signup);
+signup.propTypes = {
+  classes: PropTypes.object.isRequired,
+  user: PropTypes.object.isRequired,
+  UI: PropTypes.object.isRequired,
+  signupUser: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  user: state.user,
+  UI: state.UI
+});
+
+export default connect(mapStateToProps, { signupUser })(
+  withStyles(styles)(signup)
+);
--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -3,7 +3,8 @@ import {
   SET_ERRORS,
   CLEAR_ERRORS,
   LOADING_USER,
-  LOADING_UI
+  LOADING_UI,
+  SET_UNAUTHENTICATED
 } from "../types";
 import axios from "axios";
 
@@ -12,9 +13,30 @@ export const loginUser = (userData, history) => dispatch => {
   axios
     .post("/login", userData)
     .then(res => {
-      const FBIdToken = `Bearer ${res.data.token}`;
-      localStorage.setItem("FBIdToken", `Bearer ${res.data.token}`);
-      axios.defaults.headers.common["Authorization"] = FBIdToken;
+      setAuthorizationHeader(res.data.token);
+      dispatch(getUserData());
+      dispatch({ type: CLEAR_ERRORS });
+      history.push("/");
+    })
+    .catch(err => {
+      dispatch({ type: SET_ERRORS, payload: err.response.data });
+    });
+};
+
+export const logoutUser = () => dispatch => {
+  localStorage.removeItem("FBIdToken");
+  delete axios.defaults.headers.common["Authorization"];
+  dispatch({ type: SET_UNAUTHENTICATED });
+};
+
+export const signupUser = (newUserData, history) => dispatch => {
+  dispatch({ type: LOADING_UI });
+  console.log("ok");
+
+  axios
+    .post("/signup", newUserData)
+    .then(res => {
+      setAuthorizationHeader(res.data.token);
       dispatch(getUserData());
       dispatch({ type: CLEAR_ERRORS });
       history.push("/");
@@ -34,4 +56,10 @@ export const getUserData = () => dispatch => {
       });
     })
     .catch(err => console.log(err));
+};
+
+const setAuthorizationHeader = token => {
+  const FBIdToken = `Bearer ${token}`;
+  localStorage.setItem("FBIdToken", `Bearer ${token}`);
+  axios.defaults.headers.common["Authorization"] = FBIdToken;
 };
